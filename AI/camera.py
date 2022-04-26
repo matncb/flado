7c0fb9609c4767491cc83ae26b422a43d36df7cf
mavivaldi <mavivaldi17@gmail.com>
2022-04-26 19:50:26 -0300

    mais coisa

--- AI/camera.py
+++ AI/camera.py
@@ -1,7 +1,7 @@
 import cv2
 import serial
 import time
-pasta = './cilindros/1.6/'
+pasta = './cilindros/1.6_v2/'
 camera = cv2.VideoCapture(1)
 
 def foto(n):
@@ -11,8 +11,7 @@ def foto(n):
 
 arduino = serial.Serial('COM8', 9600)
 
-n = 85
-#muda com a foto
+n = 97 #muda com a foto
 cont = 0 #sempre assim
 
 time.sleep(2)
